51c820de07ec6b063a3f9bc245e6f9d9662b2c4a
likanstha <likan.stha@gmail.com>
2022-06-22 23:38:08 +1000

    react route on menu

--- src/App.js
+++ src/App.js
@@ -6,26 +6,46 @@ import {Dashboard} from './pages/dashboard/Dashboard.page';
 import { AddTicket } from './pages/new-ticket/AddTicket.page';
 import { TicketLists } from './pages/ticket-list/TicketLists.page';
 import { Ticket } from './pages/ticket/Ticket.page'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     
     <div className='App'>
-      
-      <DefaultLayout>
-        {/*<Dashboard />
-           <AddTicket />
-          <TicketLists />*/}
-         <Ticket />
-        </DefaultLayout>
-       
-      {/* <Entry /> */}
+      <Router>
+        <Switch>
+
+          <Route exact path="/">
+          <Entry />
+          </Route>
+          <DefaultLayout>
+          <Route path="/dashboard">
+          <Dashboard />
+          </Route>
+
+          <Route path="/add-ticket">
+          <AddTicket />
+          </Route>
+
+          <Route path="/tickets">
+          <TicketLists />
+          </Route>
+          <Route path="/ticket/:tid">
+          <Ticket />
+          </Route>
+        
+          </DefaultLayout>
+          {/* <Route path="/">
+          <Entry />
+          </Route> */}
+         
+      </Switch>
+      </Router>
       </div>
-
-
-
-
   );
 }
 export default App;
--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import logo from "../../assets/img/logo.png";
 import { Nav,Navbar } from 'react-bootstrap'
+import {  NavLink, useHistory  } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+
 export const Header = () => {
+  const history = useHistory();
+  
+  const logMeout =()=>{
+    history.push("/");
+  };
   return( 
   <Navbar  collapseOnSelect bg="info"
    variant="dark" expand="md"  >
@@ -11,9 +19,26 @@ export const Header = () => {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id ="basic-navbar-nav">
       <Nav className="ml-auto">
-        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+         {/* <Link to="/dashboard">Dashboard</Link>
+        <Link to="/tickets">Tickets</Link>
+        <Link to="">Logout</Link>  */}
+        
+        <LinkContainer to="/tickets">
+          <Nav.Link >Tickets</Nav.Link>
+        </LinkContainer> 
+         <LinkContainer to="/dashboard">
+        <Nav.Link>Dashboard</Nav.Link>
+        </LinkContainer> 
+        
+        <Nav.Link onClick={logMeout}>Logout</Nav.Link>
+       
+        
+
+          
+     
+        {/* <Nav.Link href="/dashboard">Dashboard</Nav.Link>
         <Nav.Link href="/dashboard">Tickets</Nav.Link>
-        <Nav.Link href="/dashboard">Logout</Nav.Link>
+        <Nav.Link href="/dashboard">Logout</Nav.Link> */}
  
       
       </Nav>
